e2547150ed2b80f01ead4a1d93a4816949a7d953
mstmdev <mstmdev@gmail.com>
2024-01-29 01:41:15 +0800

    添加内核更新脚本

---
+++ script/update_kernel.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "当前系统的内核版本：$(uname -rs)"
+echo "导入ELRepo仓库的公钥信息"
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+echo "安装RHEL-9 ELRepo"
+yum install https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm
+echo "当前可用的内核发行版本："
+yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+echo "开始安装长期支持版本kernel-lt"
+yum --enablerepo=elrepo-kernel install kernel-lt
+echo "当前kernel信息："
+grubby --info=ALL | grep ^kernel
+echo "当前系统的默认内核：$(grubby --default-kernel)"
+echo "更新默认kernel"
+grubby --set-default "/boot/vmlinuz-6.1.75-1.el9.elrepo.x86_64"
+
+for i in {1..10}; do
+  echo -e "\r$((10 - i + 1))秒后重启...\c"
+  sleep 1
+  if [ $i == 10 ]; then
+    reboot
+  fi
+done
---
+++ script/update_kernel_lib.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "当前系统的内核版本：$(uname -rs)"
+echo "当前内核开发包信息："
+rpm -qa | grep kernel
+
+echo "移除老的内核开发包："
+yum remove kernel-core-5.14.0-202.el9.x86_64 \
+  kernel-modules-5.14.0-202.el9.x86_64 \
+  kernel-5.14.0-202.el9.x86_64 \
+  kernel-headers-5.14.0-202.el9.x86_64 \
+  kernel-devel-5.14.0-202.el9.x86_64 \
+  kernel-tools-libs-5.14.0-202.el9.x86_64 \
+  kernel-tools-5.14.0-202.el9.x86_64
+
+echo "安装新的内核开发包："
+yum --enablerepo=elrepo-kernel install kernel-lt-devel kernel-lt-headers kernel-lt-tools kernel-lt-tools-libs kernel-lt-tools-libs-devel kernel-lt-doc gcc
+
+echo "更新软件包"
+yum --enablerepo=elrepo-kernel update
