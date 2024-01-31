11b0d72d12fec09eac54b9481a049b599a4b4a26
mstmdev <mstmdev@gmail.com>
2024-01-31 17:32:27 +0800

    修复脚本名称错误

--- script/sync/generate_cert.sh
+++ script/sync/generate_cert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source init_secert.sh
+source init_secret.sh
 
 openssl genrsa -out cert/key.pem 2048
 openssl req -new -x509 -key cert/key.pem -out cert/cert.pem -days 36500 -subj "/CN=$GOFS_SERVER_ADDR"
