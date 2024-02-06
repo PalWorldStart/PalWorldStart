964e114d8299178d84ef23accea4b6538162dfe7
mstmdev <mstmdev@gmail.com>
2024-02-06 14:04:56 +0800

    Add sudo

--- script/containerd/install_containerd.sh
+++ script/containerd/install_containerd.sh
@@ -17,28 +17,28 @@ echo "开始下载cni-plugins v$CNI_PLUGINS_VERSION"
 wget -nc https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
 
 echo "开始安装containerd v$CONTAINERD_VERSION"
-tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
 echo "开始安装nerdctl v$NERDCTL_VERSION"
-tar Cxzvvf /usr/local/bin nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
+sudo tar Cxzvvf /usr/local/bin nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
 echo "开始安装containerd.service"
-mkdir -p /usr/local/lib/systemd/system
-cp containerd.service /usr/local/lib/systemd/system/containerd.service
-systemctl daemon-reload
-systemctl enable --now containerd
+sudo mkdir -p /usr/local/lib/systemd/system
+sudo cp containerd.service /usr/local/lib/systemd/system/containerd.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
 echo "开始安装runc v$RUNC_VERSION"
-install -m 755 runc.amd64 /usr/local/sbin/runc
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 echo "开始安装cni-plugins v$CNI_PLUGINS_VERSION"
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
 
 echo "开始生成containerd默认配置文件"
-mkdir -p /etc/containerd
-containerd config default >/etc/containerd/config.toml
+sudo mkdir -p /etc/containerd
+sudo containerd config default >/etc/containerd/config.toml
 
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 echo "重启containerd"
-systemctl restart containerd
+sudo systemctl restart containerd
 
 containerd -v
 echo "containerd安装结束"
