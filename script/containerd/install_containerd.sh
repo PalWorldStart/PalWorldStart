670ecd5e3da35c881d854b3b89a188f54d8984a9
mstmdev <mstmdev@gmail.com>
2024-02-06 15:01:52 +0800

    Add GitHub workflows (#2)

--- script/containerd/install_containerd.sh
+++ script/containerd/install_containerd.sh
@@ -6,39 +6,44 @@ RUNC_VERSION=1.1.12
 CNI_PLUGINS_VERSION=1.4.0
 
 echo "开始下载containerd v$CONTAINERD_VERSION"
-wget -nc https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+wget -nc -q https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
 echo "开始下载nerdctl v$NERDCTL_VERSION"
-wget -nc https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
+wget -nc -q https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
 echo "开始下载containerd.service"
-wget -nc https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+wget -nc -q https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 echo "开始下载runc v$RUNC_VERSION"
-wget -nc https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64
+wget -nc -q https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64
 echo "开始下载cni-plugins v$CNI_PLUGINS_VERSION"
-wget -nc https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
+wget -nc -q https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
 
 echo "开始安装containerd v$CONTAINERD_VERSION"
-tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+sudo tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
 echo "开始安装nerdctl v$NERDCTL_VERSION"
-tar Cxzvvf /usr/local/bin nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
+sudo tar Cxzvvf /usr/local/bin nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
 echo "开始安装containerd.service"
-mkdir -p /usr/local/lib/systemd/system
-cp containerd.service /usr/local/lib/systemd/system/containerd.service
-systemctl daemon-reload
-systemctl enable --now containerd
+sudo mkdir -p /usr/local/lib/systemd/system
+sudo cp containerd.service /usr/local/lib/systemd/system/containerd.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
 echo "开始安装runc v$RUNC_VERSION"
-install -m 755 runc.amd64 /usr/local/sbin/runc
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 echo "开始安装cni-plugins v$CNI_PLUGINS_VERSION"
-mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
 
 echo "开始生成containerd默认配置文件"
-mkdir -p /etc/containerd
-containerd config default >/etc/containerd/config.toml
-
-sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+if [ -d "/etc/containerd" ]; then
+  CONTAINERD_BACKUP=containerd_bak_$(date +%Y%m%d%H%M%S).tar.gz
+  cd /etc && sudo tar -zcvf $CONTAINERD_BACKUP containerd && cd - && sudo mv /etc/$CONTAINERD_BACKUP .
+else
+  sudo mkdir -p /etc/containerd
+fi
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 
 echo "重启containerd"
-systemctl restart containerd
+sudo systemctl restart containerd
 
 containerd -v
 echo "containerd安装结束"
+ls -alh
