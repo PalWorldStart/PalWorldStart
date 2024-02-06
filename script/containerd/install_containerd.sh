455d089f33f3835b5b5d7831b82f3f99e7e9821a
mstmdev <mstmdev@gmail.com>
2024-02-06 13:42:03 +0800

    提取containerd相关组件的版本，并更新至最新版

--- script/containerd/install_containerd.sh
+++ script/containerd/install_containerd.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 
-echo "开始下载containerd"
-wget https://github.com/containerd/containerd/releases/download/v1.7.12/containerd-1.7.12-linux-amd64.tar.gz
-echo "开始下载nerdctl"
-wget https://github.com/containerd/nerdctl/releases/download/v1.7.2/nerdctl-1.7.2-linux-amd64.tar.gz
-echo "开始下载containerd.service"
-wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-echo "开始下载runc"
-wget https://github.com/opencontainers/runc/releases/download/v1.1.11/runc.amd64
-echo "开始下载cni-plugins"
-wget https://github.com/containernetworking/plugins/releases/download/v1.4.0/cni-plugins-linux-amd64-v1.4.0.tgz
+CONTAINERD_VERSION=1.7.13
+NERDCTL_VERSION=1.7.3
+RUNC_VERSION=1.1.12
+CNI_PLUGINS_VERSION=1.4.0
 
-echo "开始安装containerd"
-tar Cxzvf /usr/local containerd-1.7.12-linux-amd64.tar.gz
-echo "开始安装nerdctl"
-tar Cxzvvf /usr/local/bin nerdctl-1.7.2-linux-amd64.tar.gz
+echo "开始下载containerd v$CONTAINERD_VERSION"
+wget -nc https://github.com/containerd/containerd/releases/download/v$CONTAINERD_VERSION/containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+echo "开始下载nerdctl v$NERDCTL_VERSION"
+wget -nc https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
+echo "开始下载containerd.service"
+wget -nc https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+echo "开始下载runc v$RUNC_VERSION"
+wget -nc https://github.com/opencontainers/runc/releases/download/v$RUNC_VERSION/runc.amd64
+echo "开始下载cni-plugins v$CNI_PLUGINS_VERSION"
+wget -nc https://github.com/containernetworking/plugins/releases/download/v$CNI_PLUGINS_VERSION/cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
+
+echo "开始安装containerd v$CONTAINERD_VERSION"
+tar Cxzvf /usr/local containerd-$CONTAINERD_VERSION-linux-amd64.tar.gz
+echo "开始安装nerdctl v$NERDCTL_VERSION"
+tar Cxzvvf /usr/local/bin nerdctl-$NERDCTL_VERSION-linux-amd64.tar.gz
 echo "开始安装containerd.service"
 mkdir -p /usr/local/lib/systemd/system
 cp containerd.service /usr/local/lib/systemd/system/containerd.service
 systemctl daemon-reload
 systemctl enable --now containerd
-echo "开始安装runc"
+echo "开始安装runc v$RUNC_VERSION"
 install -m 755 runc.amd64 /usr/local/sbin/runc
-echo "开始安装cni-plugins"
+echo "开始安装cni-plugins v$CNI_PLUGINS_VERSION"
 mkdir -p /opt/cni/bin
-tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.4.0.tgz
+tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v$CNI_PLUGINS_VERSION.tgz
 
 echo "开始生成containerd默认配置文件"
 mkdir -p /etc/containerd
